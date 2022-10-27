366e8ebf25b8b44cb1bfd0d5d4a981dd4835d8d2
paulo.tavares <paulo.tavares@olxbr.com>
2022-10-27 19:00:51 -0300

    add cors

--- src/main/kotlin/com/higortavares/bookstore/adapter/web/response/ProductResponse.kt
+++ src/main/kotlin/com/higortavares/bookstore/adapter/web/response/ProductResponse.kt
@@ -8,12 +8,13 @@ data class ProductResponse(
     val id: Long,
     val name: String,
     val description: String,
+    val price: Int,
     @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
     val createdAt: LocalDateTime
 ){
     companion object{
         operator fun invoke(product: Product) : ProductResponse{
-            return ProductResponse(product.id!!, product.name, product.description, product.createdAt)
+            return ProductResponse(product.id!!, product.name, product.description,product.price, product.createdAt)
         }
     }
 }
